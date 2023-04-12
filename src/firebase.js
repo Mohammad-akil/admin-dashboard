a8080c0e8b2af9f740364c226978410b940295b5
Mohammad-akil <mohammadakilshakirhusain@gmail.com>
2023-04-12 13:05:42 +0530

    adding firestore for image upload

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,8 @@
 
 import { initializeApp} from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCef1B5KBsetIN7VYom-5Nrdr8Zeaw6FaE",
@@ -13,4 +15,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app)
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app)
--- src/pages/new/New.jsx
+++ src/pages/new/New.jsx
@@ -2,13 +2,90 @@ import "./New.scss";
 import Sidebar from "./../../components/Sidebar";
 import Navbar from "./../../components/Navbar";
 import DriveFolderUploadOutlinedIcon from "@mui/icons-material/DriveFolderUploadOutlined";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { serverTimestamp, setDoc, doc } from "firebase/firestore";
+import { auth, db } from "../../firebase";
+import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { storage } from "../../firebase";
 
-const New = ({inputs,title}) => {
+const New = ({ inputs, title }) => {
+  const [file, setFile] = useState(null);
+  const [data, setData] = useState();
 
-  const [file,setFile] = useState(null)
+  useEffect(() => {
+    const uploadFile = () => {
+      if (!file) return;
+      const imageName = new Date().getTime() + file.name;
+      
+      console.log("i am up");
+      const storageRef = ref(storage, `images/${imageName}`);
+      console.log("i am down");
 
- 
+      const uploadTask = uploadBytesResumable(storageRef, file);
+
+      uploadTask.on(
+        "state_changed",
+        (snapshot) => {
+          const progress =
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          console.log("Upload is " + progress + "% done");
+          switch (snapshot.state) {
+            case "paused":
+              console.log("Upload is paused");
+              break;
+            case "running":
+              console.log("Upload is running");
+              break;
+            default:
+              console.log("default case");
+              break;
+          }
+        },
+        (error) => {
+          console.log(error);
+        },
+        () => {
+          
+          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+            console.log("File available at", downloadURL);
+            setData((prev) => ({
+              ...prev,
+              img: downloadURL,
+            }));
+          });
+        }
+      );
+    };
+
+    file && uploadFile();
+  }, [file]);
+
+  const handleInput = (e) => {
+    const id = e.target.id;
+    const value = e.target.value;
+
+    setData({ ...data, [id]: value });
+  };
+
+  const handleAdd = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await createUserWithEmailAndPassword(
+        auth,
+        data.email,
+        data.password
+      );
+
+      await setDoc(doc(db, "users", res.user.uid), {
+        ...data,
+        Timestamp: serverTimestamp(),
+      });
+      console.log(res);
+    } catch (err) {
+      console.log(err);
+    }
+  };
   return (
     <div className="new">
       <Sidebar />
@@ -20,12 +97,16 @@ const New = ({inputs,title}) => {
         <div className="bottom">
           <div className="left">
             <img
-              src={file ? URL.createObjectURL(file) : 'https://icon-library.com/images/no-image-icon/no-image-icon-0.jpg'}
+              src={
+                file
+                  ? URL.createObjectURL(file)
+                  : "https://icon-library.com/images/no-image-icon/no-image-icon-0.jpg"
+              }
               alt=""
             />
           </div>
           <div className="right">
-            <form>
+            <form onSubmit={handleAdd}>
               <div className="formInput">
                 <label htmlFor="file">
                   Image: <DriveFolderUploadOutlinedIcon className="icon" />
@@ -37,15 +118,18 @@ const New = ({inputs,title}) => {
                   style={{ display: "none" }}
                 />
               </div>
-              {
-                inputs.map((input,index)=>(
+              {inputs.map((input, index) => (
                 <div className="formInput" key={index}>
-                <label>{input.label}</label>
-                <input type={input.type} placeholder={input.placeholder} />
-              </div>
-                ))
-              }
-              <button >send</button>
+                  <label>{input.label}</label>
+                  <input
+                    id={input.id}
+                    type={input.type}
+                    placeholder={input.placeholder}
+                    onChange={handleInput}
+                  />
+                </div>
+              ))}
+              <button type="submit">send</button>
             </form>
           </div>
         </div>
