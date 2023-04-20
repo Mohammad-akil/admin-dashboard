962e0b6949017c5b2ca26d0775982936541e5514
Mohammad-akil <mohammadakilshakirhusain@gmail.com>
2023-04-20 15:06:39 +0530

    resolve bugs

--- src/components/Widget.jsx
+++ src/components/Widget.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import "./Widget.scss";
 import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
 import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
